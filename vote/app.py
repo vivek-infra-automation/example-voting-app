ed24341c033e7e4dd6ae3832045e7c9b428ef306
harishrajput13 <harishrajput13032003@gmail.com>
2025-12-16 15:31:21 +0530

    adding project-1 all files

--- vote/app.py
+++ vote/app.py
@@ -1,23 +1,51 @@
-const express = require("express");
-const app = express();
+from flask import Flask, render_template, request, make_response, g
+from redis import Redis
+import os
+import socket
+import random
+import json
+import logging
 
-app.use(express.urlencoded({ extended: true }));
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
+hostname = socket.gethostname()
 
-app.get("/", (req, res) => {
-  res.send(`
-    <h1>Vote</h1>
-    <form method="POST">
-      <button name="vote" value="cats">Cats</button>
-      <button name="vote" value="dogs">Dogs</button>
-    </form>
-  `);
-});
+app = Flask(__name__)
 
-app.post("/", (req, res) => {
-  console.log("Vote:", req.body.vote);
-  res.redirect("/");
-});
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_error_logger.handlers)
+app.logger.setLevel(logging.INFO)
 
-app.listen(80, () => {
-  console.log("Vote app running on port 80");
-});
+def get_redis():
+    if not hasattr(g, 'redis'):
+        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+    return g.redis
+
+@app.route("/", methods=['POST','GET'])
+def hello():
+    voter_id = request.cookies.get('voter_id')
+    if not voter_id:
+        voter_id = hex(random.getrandbits(64))[2:-1]
+
+    vote = None
+
+    if request.method == 'POST':
+        redis = get_redis()
+        vote = request.form['vote']
+        app.logger.info('Received vote for %s', vote)
+        data = json.dumps({'voter_id': voter_id, 'vote': vote})
+        redis.rpush('votes', data)
+
+    resp = make_response(render_template(
+        'index.html',
+        option_a=option_a,
+        option_b=option_b,
+        hostname=hostname,
+        vote=vote,
+    ))
+    resp.set_cookie('voter_id', voter_id)
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
