2098d97ba9fee01bdaddeadf03ddc0af7d8ee75f
harishrajput13 <harishrajput13032003@gmail.com>
2025-12-16 09:13:23 +0530

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -1,51 +1,23 @@
-from flask import Flask, render_template, request, make_response, g
-from redis import Redis
-import os
-import socket
-import random
-import json
-import logging
+const express = require("express");
+const app = express();
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
-hostname = socket.gethostname()
+app.use(express.urlencoded({ extended: true }));
 
-app = Flask(__name__)
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Vote</h1>
+    <form method="POST">
+      <button name="vote" value="cats">Cats</button>
+      <button name="vote" value="dogs">Dogs</button>
+    </form>
+  `);
+});
 
-gunicorn_error_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers.extend(gunicorn_error_logger.handlers)
-app.logger.setLevel(logging.INFO)
+app.post("/", (req, res) => {
+  console.log("Vote:", req.body.vote);
+  res.redirect("/");
+});
 
-def get_redis():
-    if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
-    return g.redis
-
-@app.route("/", methods=['POST','GET'])
-def hello():
-    voter_id = request.cookies.get('voter_id')
-    if not voter_id:
-        voter_id = hex(random.getrandbits(64))[2:-1]
-
-    vote = None
-
-    if request.method == 'POST':
-        redis = get_redis()
-        vote = request.form['vote']
-        app.logger.info('Received vote for %s', vote)
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
-        redis.rpush('votes', data)
-
-    resp = make_response(render_template(
-        'index.html',
-        option_a=option_a,
-        option_b=option_b,
-        hostname=hostname,
-        vote=vote,
-    ))
-    resp.set_cookie('voter_id', voter_id)
-    return resp
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+app.listen(80, () => {
+  console.log("Vote app running on port 80");
+});
